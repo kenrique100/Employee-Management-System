84ed41fb892b918f63fb917af393fadd3f2b8f30
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-10-21 19:02:13 +0100

    dependency updated

---
+++ EMS (1)/EMS/src/main/java/com/Api/EMS/EmployeeManagementSyetemApplication.java
@@ -0,0 +1,13 @@
+package com.Api.EMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSyetemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSyetemApplication.class, args);
+	}
+
+}
---
+++ EMS (1)/EMS/src/test/java/com/Api/EMS/EmployeeManagementSyetemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Api.EMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSyetemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
