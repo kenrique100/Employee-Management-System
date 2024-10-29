6df002b246e49e23a4e04f8b22f68fd803585354
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-10-29 22:09:05 +0100

    updated controller changing id to string

--- EMS/src/main/java/com/Api/EMS/controller/EmployeeController.java
+++ EMS/src/main/java/com/Api/EMS/controller/EmployeeController.java
@@ -2,20 +2,20 @@ package com.Api.EMS.controller;
 
 import com.Api.EMS.model.User;
 import com.Api.EMS.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/employee")
+@RequiredArgsConstructor
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
         return ResponseEntity.ok(employeeService.getUserById(id).block());
     }
 }
