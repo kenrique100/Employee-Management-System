b5cfe1c2ed65155942fa95214da0d53f579d1c8d
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-10-29 22:13:46 +0100

    updated from jpa to mongo reactive std

--- EMS/src/main/java/com/Api/EMS/repository/TaskRepository.java
+++ EMS/src/main/java/com/Api/EMS/repository/TaskRepository.java
@@ -1,7 +1,8 @@
 package com.Api.EMS.repository;
 
 import com.Api.EMS.model.Task;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface TaskRepository extends ReactiveCrudRepository<Task, Long> {
+public interface TaskRepository extends ReactiveMongoRepository<Task, String> { // Use String for MongoDB ObjectId
+    // Additional custom query methods can be added here if needed
 }
