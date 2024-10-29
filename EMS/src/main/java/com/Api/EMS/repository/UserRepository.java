a8c2b0a5f4f74333ca566519c2923ca7d40b9626
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-10-29 22:14:07 +0100

    updated from jpa to mongo reactive std

--- EMS/src/main/java/com/Api/EMS/repository/UserRepository.java
+++ EMS/src/main/java/com/Api/EMS/repository/UserRepository.java
@@ -1,15 +1,18 @@
 package com.Api.EMS.repository;
 
 import com.Api.EMS.model.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
-public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+public interface UserRepository extends ReactiveMongoRepository<User, String> { // Use String for MongoDB ObjectId
+
+    // Method to find a user by username
+    Mono<User> findByUsername(String username);
+
+    // Check if a user exists by username
     default Mono<Boolean> existsByUsername(String username) {
         return findByUsername(username)
                 .map(user -> true)
                 .defaultIfEmpty(false);
     }
-
-    Mono<User> findByUsername(String username);
 }
