ca594c8035bca63d97e72eaaa9eadf0c36beca7d
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-10-31 05:17:10 +0100

    updated methods and helper class

--- EMS/src/main/java/com/Api/EMS/service/impl/AdminServiceImpl.java
+++ EMS/src/main/java/com/Api/EMS/service/impl/AdminServiceImpl.java
@@ -4,45 +4,56 @@ import com.Api.EMS.dto.UserDTO;
 import com.Api.EMS.model.User;
 import com.Api.EMS.repository.UserRepository;
 import com.Api.EMS.service.AdminService;
-import com.Api.EMS.utils.GUIDGenerator;
+import com.Api.EMS.utils.UserUtil;
 import com.Api.EMS.validation.UserValidation;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class AdminServiceImpl implements AdminService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     private final UserValidation userValidation;
 
-    public AdminServiceImpl(UserRepository userRepository, UserValidation userValidation) {
+    public AdminServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, UserValidation userValidation) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
         this.userValidation = userValidation;
     }
 
     @Override
     public Mono<User> createUser(UserDTO<String> userDTO) {
         userValidation.validateUser(userDTO);
-        User user = populateUserFields(new User(), userDTO);
-        user.setGuid(GUIDGenerator.generateGUID(8));
-        return userRepository.save(user);
-    }
-
-    @Override
-    public Mono<User> updateUser(String id, UserDTO<String> userDTO) {
-        userValidation.validateUser(userDTO);
-        return userRepository.findById(id)
-                .flatMap(user -> {
-                    populateUserFields(user, userDTO);
+        return userRepository.existsByUsernameAndCompanyName(userDTO.getUsername(), userDTO.getCompanyName())
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.error(new IllegalArgumentException("User with this username and company name already exists"));
+                    }
+                    User user = new User();
+                    UserUtil.populateUserFields(user, userDTO);
+                    user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
                     return userRepository.save(user);
                 });
     }
 
     @Override
-    public Mono<Void> deleteUser(String id) {
-        return userRepository.findById(id)
-                .flatMap(userRepository::delete);
+    public Mono<User> updateUser(String userId, UserDTO<String> userDTO) {
+        return userRepository.findById(userId)
+                .flatMap(user -> {
+                    UserUtil.populateUserFields(user, userDTO);
+                    if (userDTO.getPassword() != null) {
+                        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+                    }
+                    return userRepository.save(user);
+                });
+    }
+
+    @Override
+    public Mono<Void> deleteUser(String userId) {
+        return userRepository.deleteById(userId);
     }
 
     @Override
@@ -51,17 +62,7 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public Mono<User> findUserById(String id) {
-        return userRepository.findById(id);
-    }
-
-    private User populateUserFields(User user, UserDTO<String> userDTO) {
-        user.setName(userDTO.getName());
-        user.setAge(userDTO.getAge());
-        user.setGender(userDTO.getGender());
-        user.setNationalIdNumber(userDTO.getNationalIdNumber());
-        user.setDateOfEmployment(userDTO.getDateOfEmployment());
-        user.setRoles(userDTO.getRoles());
-        return user;
+    public Mono<User> getUserById(String userId) {
+        return userRepository.findById(userId);
     }
 }
