ee5f2b212dcd25744fca7010fcd157de4c8d0ad4
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-10-31 05:22:23 +0100

    refactored code to add new methods

--- EMS/src/main/java/com/Api/EMS/repository/UserRepository.java
+++ EMS/src/main/java/com/Api/EMS/repository/UserRepository.java
@@ -6,12 +6,12 @@ import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> { // Use String for MongoDB ObjectId
 
-    // Method to find a user by username
-    Mono<User> findByUsername(String username);
+    // Method to find a user by username and company name
+    Mono<User> findByUsernameAndCompanyName(String username, String companyName);
 
-    // Check if a user exists by username
-    default Mono<Boolean> existsByUsername(String username) {
-        return findByUsername(username)
+    // Check if a user exists by username and company name
+    default Mono<Boolean> existsByUsernameAndCompanyName(String username, String companyName) {
+        return findByUsernameAndCompanyName(username, companyName)
                 .map(user -> true)
                 .defaultIfEmpty(false);
     }
