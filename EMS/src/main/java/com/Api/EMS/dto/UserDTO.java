f9a0826b6d154668be3ba1ef19540471ccfd5a71
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-11-14 07:02:07 +0100

    user data

--- EMS/src/main/java/com/Api/EMS/dto/UserDTO.java
+++ EMS/src/main/java/com/Api/EMS/dto/UserDTO.java
@@ -2,16 +2,16 @@ package com.Api.EMS.dto;
 
 import com.Api.EMS.model.Role;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
-public class UserDTO<T> {
-    private T guid;
+public class UserDTO<String> {
+    private String guid;
     private String username;
     private String password;
     private List<Role> roles;
     private String name;
+    private int dateOfBirth;
     private int age;
     private String telephoneNumber;
     private String gender;
