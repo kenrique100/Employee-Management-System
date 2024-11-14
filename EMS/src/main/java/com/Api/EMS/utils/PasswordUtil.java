8988521ff2fdd21200f108168d609d2cc260dee2
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-11-14 07:04:10 +0100

    utility class

--- EMS/src/main/java/com/Api/EMS/utils/PasswordUtil.java
+++ EMS/src/main/java/com/Api/EMS/utils/PasswordUtil.java
@@ -8,9 +8,6 @@ public class PasswordUtil {
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public String encode(String rawPassword) {
-        return passwordEncoder.encode(rawPassword);
-    }
 
     public boolean matches(String rawPassword, String encodedPassword) {
         return passwordEncoder.matches(rawPassword, encodedPassword);
