9bc96fa1114a024953b7b721d1df6b5cfc865616
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-11-14 07:01:11 +0100

    handles crud specific to director

--- EMS/src/main/java/com/Api/EMS/controller/DirectorController.java
+++ EMS/src/main/java/com/Api/EMS/controller/DirectorController.java
@@ -1,55 +1,14 @@
 package com.Api.EMS.controller;
 
-import com.Api.EMS.dto.UserDTO;
 import com.Api.EMS.model.User;
 import com.Api.EMS.service.UserService;
-import com.Api.EMS.utils.ResponseUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/director")
-@RequiredArgsConstructor
-public class DirectorController {
+public class DirectorController extends UserControllerBase {
 
-    private final UserService<User> userService;
-
-    @PostMapping("/user")
-    public Mono<ResponseEntity<User>> createUser(@RequestBody UserDTO<String> userDTO) {
-        return userService.createUser(userDTO)
-                .flatMap(ResponseUtil::createSuccessResponse)
-                .onErrorResume(e -> ResponseUtil.createErrorResponse(HttpStatus.BAD_REQUEST, User.class));
-    }
-
-    @PutMapping("/user/{id}")
-    public Mono<ResponseEntity<User>> updateUser(@PathVariable String id, @RequestBody UserDTO<String> userDTO) {
-        return userService.updateUser(id, userDTO)
-                .flatMap(ResponseUtil::createSuccessResponse)
-                .switchIfEmpty(ResponseUtil.createNotFoundResponse(User.class));
-    }
-
-    @DeleteMapping("/user/{id}")
-    public Mono<ResponseEntity<Void>> deleteUser(@PathVariable String id) {
-        return userService.deleteUser(id)
-                .flatMap(deleted -> deleted
-                        ? ResponseUtil.createSuccessResponse(null)
-                        : ResponseUtil.createNotFoundResponse(Void.class));
-    }
-
-    @GetMapping("/users")
-    public Flux<ResponseEntity<User>> getAllUsers() {
-        return userService.getAllUsers()
-                .flatMap(ResponseUtil::createSuccessResponse);
-    }
-
-    @GetMapping("/user/{id}")
-    public Mono<ResponseEntity<User>> findUserById(@PathVariable String id) {
-        return userService.getUserById(id)
-                .flatMap(ResponseUtil::createSuccessResponse)
-                .switchIfEmpty(ResponseUtil.createNotFoundResponse(User.class));
+    public DirectorController(UserService<User> userService) {
+        super(userService);
     }
 }
